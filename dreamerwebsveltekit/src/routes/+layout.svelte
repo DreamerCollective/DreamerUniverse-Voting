f3854a80c7203584078ef9fcd15c43f926dc8792
Zalron <davidrlawson@optusnet.com.au>
2022-10-19 21:50:51 +1100

    added functionaity to the page, working on the mobile part

--- dreamerwebsveltekit/src/routes/+layout.svelte
+++ dreamerwebsveltekit/src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
     import "../styles/global.css"
+    import Dashboard from "$lib/Dashboard.svelte";
 </script>
 
-<div class="bg-black"></div>
+<Dashboard/>
+
+
+<footer>
+    <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div class="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left"><span class="block sm:inline">&copy; 2022 Dreamer Collective.</span> <span class="block sm:inline">All rights reserved.</span></div>
+    </div>
+</footer>
 
 <slot />
--- dreamerwebsveltekit/src/routes/+page.svelte
+++ dreamerwebsveltekit/src/routes/+page.svelte
@@ -1,12 +1,6 @@
 <script>
     import "../styles/global.css"
-    import Button from "$lib/Button.svelte";
 </script>
 
-<h1 class=" bg-gray-500 text-white font-bold py-8 px-8 text-9xl"> Welcome To Dreamer Web</h1>
-<div class="flex flex-grow">
-        <Button Title="Voting" address="/Voting" />
-        <Button Title="Execution" address="/Execution" />
-        <Button Title="Discuss" address="/Discuss" />
-</div>
+
 
