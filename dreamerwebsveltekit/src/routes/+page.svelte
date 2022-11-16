e32a1a0bc7a898e32801e213c489a0a70cbb0ceb
Zalron <davidrlawson@optusnet.com.au>
2022-11-16 21:53:33 +1100

    replaced the dashboard page with a home page Added a temple hero

--- dreamerwebsveltekit/src/routes/+page.svelte
+++ dreamerwebsveltekit/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../styles/global.css"
+    import Hero from "$lib/components/Hero.svelte";
 </script>
 
 <div class="flex flex-1 flex-col md:pl-64">
@@ -7,9 +8,7 @@
         <div class="py-6">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <!-- Replace with your content -->
-                <div class="py-4">
-                    <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
-                </div>
+                <Hero />
                 <!-- /End replace -->
             </div>
         </div>
