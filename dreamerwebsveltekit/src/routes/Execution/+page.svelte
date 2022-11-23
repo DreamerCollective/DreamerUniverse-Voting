174714d323f339bf2355c1acf43b4aa41a6f68ad
Zalron <davidrlawson@optusnet.com.au>
2022-11-23 17:08:20 +1100

    added a caldendar to execution
    first functionality of execution is for a task tracker on a caldendar

--- dreamerwebsveltekit/src/routes/Execution/+page.svelte
+++ dreamerwebsveltekit/src/routes/Execution/+page.svelte
@@ -1,18 +1,7 @@
 <script>
-    export let v;
+    import DayCalendars from "$lib/components/DayCalendars.svelte";
+
+
 </script>
 
-<div class="flex flex-1 flex-col md:pl-64">
-    <main class="flex-1">
-        <div class="py-6">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                <!-- Replace with your content -->
-                <div class="py-4">
-                    <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
-                    {v}
-                </div>
-                <!-- /End replace -->
-            </div>
-        </div>
-    </main>
-</div>
+<DayCalendars />
