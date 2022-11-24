cd43f2e8d3754612909a8062d1a37eb1a7a5a22d
Zalron <davidrlawson@optusnet.com.au>
2022-11-24 20:10:21 +1100

    added profile page

--- dreamerwebsveltekit/src/lib/components/Users.svelte
+++ dreamerwebsveltekit/src/lib/components/Users.svelte
@@ -48,7 +48,7 @@
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Linme</td>
                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                <a href="/" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                             </td>
                         </tr>
 
