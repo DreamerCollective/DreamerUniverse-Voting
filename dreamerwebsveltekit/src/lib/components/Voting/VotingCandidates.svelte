3e0ea0340a7d641d8ba59c8546ffc7a58329139a
Zalron <davidrlawson@optusnet.com.au>
2023-10-30 22:20:17 +1100

    Clean up of adding dreamer roll style boxes

--- dreamerwebsveltekit/src/lib/components/Voting/VotingCandidates.svelte
+++ dreamerwebsveltekit/src/lib/components/Voting/VotingCandidates.svelte
@@ -17,13 +17,14 @@
 
 <div class="relative inline-block text-left">
     <div class="relative block rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-        <button type="button" on:click={toggleButton2} class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-            Candidates
-            <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-            </svg>
-        </button>
-    </div>
+        <div>
+            <button type="button" on:click={toggleButton2} class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                Candidates
+                <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
 
     <!--
       Dropdown menu, show/hide based on menu state.
@@ -36,11 +37,10 @@
         To: "transform opacity-0 scale-95"
     -->
 
-
-
     {#if button2}
         {#each Candidates as ComboboxCandidates}
             <VotingCandidatesCombobox ComboboxCandidates = "{ComboboxCandidates}" />
         {/each}
     {/if}
+    </div>
 </div>
